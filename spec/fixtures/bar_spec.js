75d5c0878bb56ccd74380c39e4a636a6c1b63a17
Pat Nakajima <patnakajima@gmail.com>
2008-08-30 15:53:52 -0400

    bliggety bam\!

---
+++ spec/fixtures/bar_spec.js
@@ -0,0 +1,7 @@
+Screw.Unit(function(){
+  describe("bar", function() {
+    it("foo is true", function() {
+      expect(bar.foo).to(be_true);
+    });
+  });
+});
--- spec/fixtures/foo_spec.js
+++ spec/fixtures/foo_spec.js
@@ -21,11 +21,5 @@ Screw.Unit(function() {
       throw new Error('whoops!');
     });
   });
-  
-  describe("bar", function() {
-    it("foo is true", function() {
-      expect(bar.foo).to(be_true);
-    });
-  });
 });
 
