6339f12f8bb56a1e26c54ba2982e1fa7c8451d22
Pat Nakajima <patnakajima@gmail.com>
2008-08-30 15:43:46 -0400

    cleaned up runner file

--- lib/screw_driver.rb
+++ lib/screw_driver.rb
@@ -9,43 +9,17 @@ require 'screw_driver/helpers.rb'
 configure do
   Rack::CommonLogger.class_eval { def <<(str) end } # Inhibit logging retardase
   SUITE = Screw::Driver::Suite.new(ARGV[0], self)
-  browser = ARGV[1] || 'Firefox'
-  Thread.new { sleep 1; system "open -a #{browser} 'http://localhost:4567/?body%20%3E%20.describe'" }
+  brwsr = ARGV[1] || 'Firefox'
+  Thread.new { sleep 1; system "open -a #{brwsr} 'http://localhost:4567/?body%20%3E%20.describe'" }
 end
-
-post('/passed')  { report '.'.green }
-post('/failed')  { report 'F'.red, params }
-post('/errored') { report 'E'.magenta, params }
 
 SUITE.generate_js_urls
 SUITE.generate_css_urls
 
-get '/' do
-  SUITE.send(:doc).to_s
-end
-
-post '/before' do
-  padded do
-    puts "Staring to run test suite..."
-  end
-end
-
-post '/after' do
-  padded do
-    SUITE.failures.each do |failure|
-      padded do
-        puts "FAILURE".red
-        puts "- #{failure[:name]}: #{failure[:reason]}"
-      end
-    end
-    print "Finished. #{SUITE.test_count} tests. "
-    print "#{SUITE.failures.length} failures." unless SUITE.failures.empty?
-  end
-end
-
-post '/exit' do
-  $stdout.print "== Spec Server "
-  $stdout.flush
-  pid = `ps aux | grep #{File.basename(__FILE__)} | grr --column 2`
-  `kill #{pid}`
-end
+get('/') { SUITE.send(:doc).to_s }
+post('/passed')  { report '.'.green }
+post('/failed')  { report 'F'.red, params }
+post('/errored') { report 'E'.magenta, params }
+post('/before') { before_suite }
+post('/after') { after_suite }
+post('/exit') { exit_suite }
--- lib/screw_driver/helpers.rb
+++ lib/screw_driver/helpers.rb
@@ -12,4 +12,30 @@ helpers do
     $stdout.flush
     :ok
   end
+  
+  def before_suite
+    padded do
+      puts "Staring to run test suite..."
+    end
+  end
+  
+  def after_suite
+    padded do
+      SUITE.failures.each do |failure|
+        padded do
+          puts "FAILURE".red
+          puts "- #{failure[:name]}: #{failure[:reason]}"
+        end
+      end
+      print "Finished. #{SUITE.test_count} tests. "
+      print "#{SUITE.failures.length} failures." unless SUITE.failures.empty?
+    end
+  end
+  
+  def exit_suite
+    $stdout.print "== Spec Server "
+    $stdout.flush
+    pid = `ps aux | grep #{File.basename($0)} | grr --column 2`
+    `kill #{pid}`
+  end
 end
