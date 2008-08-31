88f2667043a7887bb7bc568cb23d67d9477d1d8f
Pat Nakajima <patnakajima@gmail.com>
2008-08-31 18:14:33 -0400

    refactored js a bit. trying to figure out why result of first test isn't being reported properly.

--- spec/fixtures/spec_helper.js
+++ spec/fixtures/spec_helper.js
@@ -1,42 +1,43 @@
-$(Screw).bind('before', function() { $.post('/before') });
-$(Screw).ready(function() {
-  var results = [ ];
+var results = [ ];
 
-  var report = function() {
-    var result = results.shift();
-    if (!result) { return; }
+var report = function() {
+  var result = results.shift();
+  if (!result) { return; }
 
-    if (result.passed) {
-      return $.post(result.url, {
-        name: (result.event ? jQuery(result.event.target).find('h2').text() : ''),
-        reason: $.print(result.info)
-      });
-    }
+  if (result.passed) {
+    return $.post(result.url, {
+      name: (result.event ? jQuery(result.event.target).find('h2').text() : ''),
+      reason: $.print(result.info)
+    });
+  }
 
-    // ERROR
-    if (result.info.line) {
-      return $.post('/errored', {
-        name: jQuery(result.event.target).find('h2').text(),
-        reason: result.info
-      });
-    }
-
-    // FAILURE
-    $.post(result.url, {
+  // ERROR
+  if (result.info.line) {
+    return $.post('/errored', {
       name: jQuery(result.event.target).find('h2').text(),
       reason: result.info
     });
   }
 
-  window.setInterval(report, 50);
-  
-  var finish = function() {
-    if (results.length > 0) { return window.setTimeout(finish, 100); }
-    $.post('/after');
-    window.setTimeout(function() { $.post('/exit') }, 200);
-  }
-  
-  $(Screw).bind('after', function() { window.setTimeout(finish, 500); });
+  // FAILURE
+  $.post(result.url, {
+    name: jQuery(result.event.target).find('h2').text(),
+    reason: result.info
+  });
+}
+
+window.setInterval(report, 50);
+
+var finish = function() {
+  if (results.length > 0) { return window.setTimeout(finish, 100); }
+  $.post('/after');
+  window.setTimeout(function() { $.post('/exit') }, 200);
+}
+
+$(Screw).bind('before', function() { $.post('/before') });
+$(Screw).bind('after', function() { window.setTimeout(finish, 500); });
+
+$(Screw).ready(function() {
   $('.it').bind('passed', function(e) { results.push({ url: '/passed', passed: true }); });
   $('.it').bind('failed', function(e, reason) { results.push({ url: '/failed', info: reason, event: e }) });
 });
