22ed33167fdea3412bdf6546fe25ea0d364e4ce8
Pat Nakajima <patnakajima@gmail.com>
2008-08-31 23:58:24 -0400

    fixed spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ FIXTURE_PATH = File.expand_path(File.join(File.dirname(__FILE__), 'fixtures'))
 
 require 'rubygems'
 require 'spec'
-require File.join(File.dirname(__FILE__), '..', 'lib', 'screw_driver', 'suite.rb')
+require File.join(File.dirname(__FILE__), '..', 'lib', 'suite.rb')
 
 def create_suite(*additional_options)
   @stub = stub('context')
