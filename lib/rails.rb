47795b888fb3ba1a1bd199e13a72204fe4e1231f
Josh Knowles <josh@weplay.com>
2008-09-06 19:47:37 -0400

    Simplfy the generation of rails_urls

--- lib/rails.rb
+++ lib/rails.rb
@@ -4,21 +4,21 @@ module Screw
       def rails?
         @rails
       end
-      
+
       def rails_urls
-        @rails_urls ||= begin
-          @public_path = File.join(Dir.pwd, 'public', 'javascripts')
-          Dir.new(@public_path).entries.inject([]) do |files, entry|
-            files << File.expand_path(File.join(@public_path, entry)) if entry.match(/\.js$/)
-            files
-          end
-        end
+        @rails_urls ||= Dir[File.join(public_path, "**", "*.js")]
       end
-      
+
       def generate_rails_urls
         rails_urls.each do |url|
-          generate "/javascripts/#{File.basename(url)}", 'text/javascript', @public_path
+          generate "/javascripts/#{File.basename(url)}", 'text/javascript', public_path
         end
+      end
+
+    private
+
+      def public_path
+        @public_path ||= File.join(Dir.pwd, 'public', 'javascripts')
       end
     end
   end
